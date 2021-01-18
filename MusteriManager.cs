8806eae2cb7f3a9b336bd95dacb41555fba6a7d7
Dorukhan Cosgun <66283837+dorukhancosgun@users.noreply.github.com>
2021-01-19 00:29:42 +0100

    Add project files.

---
+++ MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriNo + "--> Numarali müşteri başarıyla eklendi");
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriNo + "--> Numarali müşteri başarıyla silindi.");
+        }
+        public void Listele(Musteri musteri)
+        {
+            
+
+            Console.WriteLine("Musteri No:" + musteri.MusteriNo + "\n" + "Musteri Adi:" + musteri.Adi + "\n" + "Musteri Soyadi:" + musteri.Soyadi);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using ClassMetotDemo;
+using System;
+
+namespace MetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriNo = 123456;
+            musteri1.Adi = "Stanley";
+            musteri1.Soyadi = "Kubrick";
+
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriNo = 654321;
+            musteri2.Adi = "Andrey";
+            musteri2.Soyadi = "Tarkovsky";
+
+
+            Musteri musteri3 = new Musteri();
+            musteri3.MusteriNo = 354521;
+            musteri3.Adi = "David";
+            musteri3.Soyadi = "Lynch";
+
+            //Array for DRY coding
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            
+            
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("------MÜŞTERİ BİLGİLERİ------");
+                Console.WriteLine("MÜŞTERİ No:" + musteri.MusteriNo + "\nMÜŞTERİ ADI :" + musteri.Adi + "\nMÜŞTERİ SOYADI :" + musteri.Soyadi);
+                Console.WriteLine("-------------********************-------------");
+            }
+            //Modules involving the console
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri3);
+            musteriManager.Sil(musteri2);
+
+            Console.WriteLine("---------------MUSTERI LISTESI---------------");
+            Console.WriteLine("1. Musteri:");
+            musteriManager.Listele(musteri1);
+
+            Console.WriteLine("2. Musteri:");
+            musteriManager.Listele(musteri3);
+            Console.ReadLine();
+        }
+        
+    }
+}
